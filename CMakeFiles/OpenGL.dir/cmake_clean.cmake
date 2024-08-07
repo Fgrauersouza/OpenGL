7d341c5a8bfdfecd5e76fc11047cf9399eee794a
Fábio Souza <37919743+Fgrauersouza@users.noreply.github.com>
2024-08-06 23:54:45 -0300

    Commit inicial
    CMake no inicio, código também no início

---
+++ CMakeFiles/OpenGL.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGL.dir/main.cpp.o"
+  "CMakeFiles/OpenGL.dir/main.cpp.o.d"
+  "OpenGL"
+  "OpenGL.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <GLFW/glfw3.h>
+
+int main(int, char**) {
+
+    GLFWwindow* window;
+
+    if (!glfwInit()) {
+        return -1;
+    }
+    
+    window = glfwCreateWindow(800, 600, "window", NULL, NULL);
+    glfwMakeContextCurrent(window);
+
+    /*
+    if (!gladLoadGLLoader(GLADloadproc)glfwGetProcAddress)){
+        std::cout << "Couldn't load opengl" << std::endl;
+        glfwTerminate();
+        return -1;
+    
+    }
+    */
+
+   //glClearColor(0.25f. 0.75f, 1.0f);
+
+    while (!glfwWindowShouldClose(window))  {
+        glfwPollEvents();
+
+        //glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+
+    }
+
+    glfwTerminate();
+
+
+}
